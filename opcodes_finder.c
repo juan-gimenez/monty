5fa026b2df99301ee7ce94c60af78cfa724a1565
Diego Monfort <diegomonfort12346@gmail.com>
2022-01-13 11:24:24 -0800

    Opcodes Finder (have errors)

---
+++ opcodes_finder.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * opcfinder -Get the opcodes
+ * @opcode: opcode
+ * @stack: stack
+ * @line_number: the .m line
+ *  Return: 0
+ */
+int opcfinder(const char *opcode, stack_t **stack, unsigned int line_number)
+{
+	int i = 0;
+
+	instruction_t opcodelist[] = {
+		{"pall", pall},
+		{"push", push},
+		{"nop", nop},
+	};
+
+	int size = sizeof(opcodelist) / sizeof(instruction_t);
+
+	while (i < size)
+	{
+		if (strcmp(opcode, opcodelist[i].opcode) == 0)
+		{
+			opcodelist[i].f(stack, line_number);
+			return (0);
+		}
+		i++;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\12", line_number, opcode);
+	exit(EXIT_FAILURE);
+}
