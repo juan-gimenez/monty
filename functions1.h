f5f0080f661e7f787af7cd11df5731a91c3cf11f
juan-gimenez <juancitogimenez0@gmail.com>
2022-01-12 19:44:27 -0800

    fixed main and deleted juan's push

--- functions1.h
+++ functions1.h
@@ -1,28 +1,5 @@
 #include "monty.h"
 
-
-/**
- * push - push data
- * @head: point to pointer to struct
- * @numb: line numb
- */
-void _push(stack_t **head, unsigned int numb)
-{
-  stack_t *newnode = NULL;
-
-  newnode = malloc(sizeof(stack_t));
-  newnode->n = glob.dato;
-  newnode->prev = NULL;
-  newnode->next = NULL;
-
-  if ((*head)->next)
-    {
-      (*head)->prev = newnode;
-      newnode->next = *head;
-    }
-  *head = newnode;
-}
-
 /**
  * _pall - prints the structure
  * @head: point to pointer to structure
--- monty.c
+++ monty.c
@@ -8,21 +8,6 @@
  **/
 int main(int argc, char **argv)
 {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-	stack_t *head;
-
-	(void)argv;
-	stack_initialize(&head);
-	if (argc != 2) /** Checks if one or more arg*/
-	{
-		printf("USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	exit(EXIT_SUCCESS);
-=======
->>>>>>> 70f106858452f5a14e1f6fb033a82cf22e4505b4
   int open, read, getresult;
   char *buffer = NULL;
   size_t size = 0;
@@ -39,7 +24,7 @@ int main(int argc, char **argv)
   if (argc != 2) /** Checks if one or more arg*/
     {
       printf(stderr, "USAGE: monty file\n");
-      return(EXIT_FAILURE);
+      exit(EXIT_FAILURE);
     }
   open = fopen(argv[1], O_RDONLY);
   if (open < 0)
@@ -59,8 +44,4 @@ int main(int argc, char **argv)
   free(stack);
   fclose(open);
   return (1); /** success **/
-<<<<<<< HEAD
-=======
->>>>>>> e66cdcfdfdbdb0b39b23e382abef81429e243abf
->>>>>>> 70f106858452f5a14e1f6fb033a82cf22e4505b4
 }
